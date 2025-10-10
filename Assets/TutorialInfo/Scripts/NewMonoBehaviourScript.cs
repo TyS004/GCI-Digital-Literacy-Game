b42a49b6d47f992ba1799ee02daa5f2ea3c2594c
Ty <tylerstier13@gmail.com>
2025-10-10 14:05:22 -0700

    UI

--- Assets/TutorialInfo/Scripts/NewMonoBehaviourScript.cs
+++ Assets/TutorialInfo/Scripts/NewMonoBehaviourScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public GameObject obj;
+    public int velocity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
