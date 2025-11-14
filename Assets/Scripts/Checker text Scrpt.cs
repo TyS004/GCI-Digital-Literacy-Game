7c13af10ef658648107cc36e3d18dcc835dd4750
William Csocarelli <wcoscarelli@chapman.edu>
2025-11-14 13:29:51 -0800

    this will have error

---
+++ Assets/Scripts/Checker text Scrpt.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CheckertextScrpt : MonoBehaviour
+{
+    public MainEmail MainEmail;
+   //public bool CheckGrammerOnClick()
+   // {
+       // foreach (string descrepency in MainEmail.GetGrammerDiscrepencies())
+    //    {
+       //     if (descrepency == MainEmail.GetHighLightedWord())
+      //      {
+    //            return true;
+       //     }
+      //  }true
+     //   return false;
+   // }
+}
+
