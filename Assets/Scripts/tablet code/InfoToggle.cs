23e7a7bb6a22760f67e7eab7b3c6b7da05e1e5e6
Joey Shoda <shoda@chapman.edu>
2026-03-20 13:06:47 -0700

    Added toggle boxes for IPad, removed next/back button where not needed.

---
+++ Assets/Scripts/tablet code/InfoToggle.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class InfoToggle : MonoBehaviour
+{
+    public GameObject infoPanel;
+    public TMP_Text arrowText;
+
+    private bool isOpen = false;
+
+    public void OnArrowClicked()
+    {
+        isOpen = !isOpen;
+        infoPanel.SetActive(isOpen);
+        arrowText.text = isOpen ? "▼" : "►";
+    }
+}
