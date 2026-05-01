f18d3efe0619660b305bacba04a1e8f2df3cafa5
Joshua Pham <joshpham@chapman.edu>
2026-05-01 12:28:51 -0700

    Can select and check profile

--- Assets/Scripts/Email_Scripts/ProfileImageManager.cs
+++ Assets/Scripts/Email_Scripts/ProfileImageManager.cs
@@ -15,15 +15,10 @@ public class ProfileImageManager : MonoBehaviour
         profileSprites = Resources.LoadAll<Sprite>(path);
     }
 
-    public Sprite GetProfile(string email)
+    public Sprite GetProfile(string profile)
     {
-        // dont check for domain, either reformat the levsl syntax to have a "Profile" section at the top of each email that takes in teh file name of the profile image
-        // or have it look for the name+domain in one string that we then set the names of the profile image files to
-        // probably the first one is better and easier
-        string domain = email.Contains("@") ? email.Split('@')[1].Split('.')[0].ToLower() : email.ToLower();
-        
         foreach (Sprite sprite in profileSprites)
-            if (sprite.name == domain)
+            if (sprite.name == profile)
                 return sprite;
             
         return DefaultSprite;
